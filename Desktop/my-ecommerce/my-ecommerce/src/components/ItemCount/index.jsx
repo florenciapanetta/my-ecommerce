1856294befdddd62a0d6be17366d6a8ce4b4ba20
Flor <fp-@hotmail.com.ar>
2023-03-20 20:50:13 -0300

    Fallas con CSS ItemCount

---
+++ Desktop/my-ecommerce/my-ecommerce/src/components/ItemCount/index.jsx
@@ -0,0 +1,19 @@
+import './ItemCount.css';
+
+import React from 'react';
+
+export const ItemCount = () => {
+
+  return (
+    <div className= 'contador'>
+     <button></button>
+     <span>2</span>
+     <button></button>
+     <div>
+     <button>Agregar al carrito</button>
+     </div>
+    </div>
+  );
+}
+
+export default ItemCount;
--- Desktop/my-ecommerce/my-ecommerce/src/components/ItemListContainer/index.jsx
+++ Desktop/my-ecommerce/my-ecommerce/src/components/ItemListContainer/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import Title from '../Title'
+import React from 'react';
+import Title from '../Title';
+import ItemCount from '../ItemCount';
+
 
 export const ItemListContainer = ({ texto }) => {
   return (
+    <>
     <Title greeting={texto} />
+    <ItemCount  />
+    </>
   )
 }
 
